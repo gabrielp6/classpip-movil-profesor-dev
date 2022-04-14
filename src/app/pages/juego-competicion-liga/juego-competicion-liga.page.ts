30ddb160e249a6694f1b3410c6e8ce0495d492cc
Suhail-Chaib <abc_suhail@hotmail.com>
2022-04-14 02:26:40 +0200

    Fixing Merge Problems

--- src/app/pages/juego-competicion-liga/juego-competicion-liga.page.ts
+++ src/app/pages/juego-competicion-liga/juego-competicion-liga.page.ts
@@ -93,7 +93,7 @@ export class JuegoCompeticionLigaPage implements OnInit {
     this.navCtrl.navigateForward('/informacion-jornadas');
   }
 
-  /*DameJornadasDelJuegoDeCompeticionSeleccionado() {
+  DameJornadasDelJuegoDeCompeticionSeleccionado() {
     this.peticionesAPI.DameJornadasDeCompeticionLiga(this.juegoSeleccionado.id)
       .subscribe(inscripciones => {
         this.jornadas = inscripciones;
@@ -274,8 +274,7 @@ export class JuegoCompeticionLigaPage implements OnInit {
     console.log('Las tablas JornadasCompeticionLiga son: ');
     console.log(this.JornadasCompeticion);
     console.log ('Voy a por la información de las jornadas del juego');
-    this.sesion.TomaDatosJornadas(this.jornadas,
-                                      this.JornadasCompeticion);
+    this.sesion.TomaDatosJornadas(this.jornadas,this.JornadasCompeticion);
     this.sesion.TomaTablaAlumnoJuegoDeCompeticion(this.rankingAlumnoJuegoDeCompeticion);
     this.sesion.TomaTablaEquipoJuegoDeCompeticion(this.rankingEquiposJuegoDeCompeticion);
     this.sesion.TomaAlumno (this.MiAlumno);
@@ -283,24 +282,12 @@ export class JuegoCompeticionLigaPage implements OnInit {
     this.navCtrl.navigateForward('/informacion-jornadas');
   }
 
-  /*EditarJornadas(){
-    this.navCtrl.navigateForward('/editar-jornadas');
-  }*/
-
-
-
   EditarJornadas() {
 
-    console.log('Tomo las jornadas' + this.jornadas);
-    console.log ('Aquí estará la información del juego');
+    console.log("hola k hace" + this.jornadas);
     this.sesion.TomaJuego (this.juegoSeleccionado);
-    this.JornadasCompeticion = this.calculos.GenerarTablaJornadasLiga(this.juegoSeleccionado, this.jornadas, this.enfrentamientosDelJuego);
-    console.log('Juego activo' + this.JornadasCompeticion);
-
-    this.sesion.TomaDatosJornadas(
-      this.jornadas,
-      this.JornadasCompeticion
-    );
+    this.calculos.GenerarTablaJornadasLiga(this.juegoSeleccionado, this.jornadas, this.enfrentamientosDelJuego);
+    this.sesion.TomaDatosJornadas(this.jornadas,this.JornadasCompeticion);
     
     this.navCtrl.navigateForward('/editar-jornadas');
   }
